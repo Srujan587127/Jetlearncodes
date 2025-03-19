000c49f713b4db54eeed4d1843c81ea645f437fd
Srujan587127 <srkallem09@gmail.com>
2025-03-18 20:05:27 -0400

    Update galaga.py

--- galaga.py
+++ galaga.py
@@ -4,25 +4,25 @@ import random
 WIDTH = 1200
 HEIGHT = 600
 
-WHITE = (255,255,255)
-BLUE = (0,0,255)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
 
 ship = Actor("galaga")
 bug = Actor("bug")
 
-ship.pos = (WIDTH//2 ,  HEIGHT-60)
+ship.pos = (WIDTH // 2, HEIGHT - 60)
 
 speed = 5
 
 bullets = []
-
 enemies = []
 
 for x in range(8):
     for y in range(4):
-        enemies.append(Actor("bug"))
-        enemies[-1].x = 100 + 50*x 
-        enemies[-1].y = 80 +50*y
+        enemy = Actor("bug")
+        enemy.x = 100 + 50 * x
+        enemy.y = 80 + 50 * y
+        enemies.append(enemy)
 
 score = 0
 direction = 1
@@ -30,79 +30,82 @@ ship.dead = False
 ship.countdown = 90
 
 def displayScore():
-    screen.draw.text(str(score), (50,30))
+    screen.draw.text(str(score), (50, 30), color=WHITE, fontsize=40)
 
 def gameOver():
-    screen.draw.text("GAME OVER", (250,250))
+    screen.draw.text("GAME OVER", (500, 250), color=WHITE, fontsize=60)
 
 def on_key_down(key):
-    if ship.dead == False:
+    if not ship.dead:
         if key == keys.SPACE:
-            bullets.append(Actor("bullet"))
-            bullets[-1].x = ship.x
-            bullets[-1].y = ship.y - 50
+            bullet = Actor("bullet")
+            bullet.x = ship.x
+            bullet.y = ship.y - 50
+            bullets.append(bullet)
 
 def update():
-    global score
-    global direction
+    global score, direction
     moveDown = False
 
-    if ship.dead == False:
+    if not ship.dead:
         if keyboard.left:
             ship.x -= speed
-            if ship.x <=0:
+            if ship.x <= 0:
                 ship.x = 0
 
-        elif keyboard.right:
+        if keyboard.right:
             ship.x += speed
-            if ship.x >=WIDTH:
-                ship.x = WIDTH 
+            if ship.x >= WIDTH:
+                ship.x = WIDTH
 
-for bullet in bullets:
-    if bullet.y <=0 :
-        bullets.remove(bullet)
-    else:
-        bullet.y -= 10
-
-if len(enemies) == 10:
-    gameOver()
-
-if len(enemies)>0 and (enemies[-1].x > WIDTH - 80 or enemies[0].x < 80):
-    moveDown = True
-    direction = direction - 1
-for enemy in enemies:
-    enemy.x += 5*direction
-    if moveDown == True:
-        enemy.y += 70
-    if enemy.y > HEIGHT:
-        enemies.remove(enemy)
-
-    for bullet in bullets :
-        if enemy.colliderect(bullet):
-            score +=100
+    for bullet in bullets[:]:  
+        if bullet.y <= 0:
             bullets.remove(bullet)
+        else:
+            bullet.y -= 10
+
+    if len(enemies) == 10:
+        gameOver()
+
+    if len(enemies) > 0 and (enemies[-1].x > WIDTH - 80 or enemies[0].x < 80):
+        moveDown = True
+        direction *= -1
+
+    for enemy in enemies[:]:  
+        enemy.x += 5 * direction
+        if moveDown:
+            enemy.y += 70
+        if enemy.y > HEIGHT:
             enemies.remove(enemy)
-            if len(enemies) == 0:
-                game0ver()
-    if enemy.colliderect(ship):
-        ship.dead = True 
-if ship.dead:
-    ship.countdown -=1
-if ship.countdown == 0 :
-    ship.dead = False
-    ship.countdown = 90
+
+        for bullet in bullets[:]:
+            if enemy.colliderect(bullet):
+                score += 100
+                bullets.remove(bullet)
+                enemies.remove(enemy)
+                break  
+
+        if enemy.colliderect(ship):
+            ship.dead = True
+
+    if ship.dead:
+        ship.countdown -= 1
+        if ship.countdown == 0:
+            ship.dead = False
+            ship.countdown = 90
 
 def draw():
     screen.clear()
-    screen.fill(blue)
+    screen.fill(BLUE)
 
     for bullet in bullets:
         bullet.draw()
     for enemy in enemies:
         enemy.draw()
-    if ship.dead == False:
+    if not ship.dead:
         ship.draw()
     displayScore()
     if len(enemies) == 0:
         gameOver()
+
 pgzrun.go()
